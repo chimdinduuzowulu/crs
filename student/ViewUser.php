2e2e83bf3abcdfca2c579de9b7bb692c265209c6
chimdinduuzowulu <53602449+chimdinduuzowulu@users.noreply.github.com>
2022-05-09 19:39:07 +0100

    second commit

---
+++ student/ViewUser.php
@@ -0,0 +1,53 @@
+
+<?php
+session_start();
+include_once '../connection.php';
+$email = $_SESSION['email'];
+$password = $_SESSION['password'];
+$sql="SELECT * FROM user where email = '$email' AND password = '$password'";
+$res=mysqli_query($con,$sql) or die(mysqli_error($con));
+?>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<style type="text/css">
+#viewdata table{
+    border:1px solid #aaa;}
+#viewdata th{background:#aaa;}
+#viewdata td{background:#efefef;}
+#viewdata th,td{padding:5px;text-align:left;}
+</style>
+<table id="viewdata" class="table table-striped">
+<tr>
+<th>#</th>
+<th>First name</th>
+<th>Last name</th>
+<th>Phone number</th>
+<th>Email Address</th>
+<th>User Type</th>
+<th>Action</th>
+</tr>
+<?php
+while($row=mysqli_fetch_assoc($res))
+{
+echo "<tr><td>";
+echo $row['id'];
+echo "</td><td>";
+echo $row['fname'];
+echo "</td><td>";
+echo $row['lname'];
+echo "</td><td>";
+echo $row['phone'];
+echo "</td><td>";
+echo $row['email'];
+echo "</td><td>";
+echo $row['type'];
+echo "
+<td><a href=\"#\" class=\" btn btn-primary\" onclick=\"getPage('../nbproject/Edit.php?data=".$row['id']."')\">Edit</a></td></tr>";
+}
+mysqli_close($con);
+?>
+</table>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
